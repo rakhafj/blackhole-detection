4ee6102f7740594b53bd7a99afb2cac593e14a29
rakhafj <rakhafikran@gmail.com>
2022-08-30 14:23:22 +0700

    First Commit

---
+++ 20nodes/test.tcl
@@ -0,0 +1,10 @@
+# 802.11p default parameters
+Phy/WirelessPhyExt set CSThresh_ 3.9810717055349694e-13 ;# -94 dBm wireless interface sensitivity
+Phy/WirelessPhyExt set Pt_ 0.0005
+;# equals 20dBm when considering antenna
+gains of 1.0
+Phy/WirelessPhyExt set freq_ 5.9e+9
+Phy/WirelessPhyExt set noise_floor_ 1.26e-13
+;# -99 dBm for 10MHz bandwidth
+Phy/WirelessPhyExt set L_ 1.0
+;# default radio circuit gain/loss
